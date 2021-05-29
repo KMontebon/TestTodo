c6a486da1360d7bbc09c1b9ca63fb8ff996d7394
KMontebon <kmontebon_190000001369@uic.edu.ph>
2021-05-29 14:52:47 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,232 @@
+const express = require("express");
+const { pool } = require("./dbConfig");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const cors = require("cors");
+const session = require("express-session");
+require("dotenv").config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const initializePassport = require("./passportConfig");
+
+initializePassport(passport);
+
+var role;
+
+// Middleware
+
+// Parses details from a form
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+
+app.use(
+  session({
+    // Key we want to keep secret which will encrypt all of our information
+    secret: process.env.SESSION_SECRET,
+    // Should we resave our session variables if nothing has changes which we dont
+    resave: false,
+    // Save empty value if there is no vaue which we do not want to do
+    saveUninitialized: false
+  })
+);
+// Funtion inside passport which initializes passport
+app.use(passport.initialize());
+// Store our variables to be persisted across the whole session. Works with app.use(Session) above
+app.use(passport.session());
+app.use(flash());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/users/register", checkAuthenticated, (req, res) => {
+  res.render("register.ejs");
+});
+
+app.get("/users/login", checkAuthenticated, (req, res) => {
+  // flash sets a messages variable. passport sets the error message
+  console.log(req.session.flash.error);
+  res.render("login.ejs");
+});
+
+app.get("/users/dashboard", checkNotAuthenticated, (req, res) => {
+  console.log(req.isAuthenticated());
+  res.render("dashboard", { user: req.user.name });
+});
+
+app.get("/users/logout", (req, res) => {
+  req.logout();
+  res.render("index", { message: "You have logged out successfully" });
+});
+
+app.post("/users/register", async (req, res) => {
+  let { name, email, password, password2 } = req.body;
+
+  let errors = [];
+
+  console.log({
+    name,
+    email,
+    password,
+    password2
+  });
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: "Please enter all fields" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ message: "Password must be a least 6 characters long" });
+  }
+
+  if (password !== password2) {
+    errors.push({ message: "Passwords do not match" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", { errors, name, email, password, password2 });
+  } else {
+    hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+    // Validation passed
+    pool.query(
+      `SELECT * FROM users
+        WHERE email = $1`,
+      [email],
+      (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(results.rows);
+
+        if (results.rows.length > 0) {
+          errors.push({ message: "Email already registered"});
+          res.render("register", { errors });
+        } else {
+          pool.query(
+            `INSERT INTO users (name, email, password)
+                VALUES ($1, $2, $3)
+                RETURNING id, password`,
+            [name, email, hashedPassword],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              console.log(results.rows);
+              req.flash("success_msg", "You are now registered. Please log in");
+              res.redirect("/users/login");
+            }
+          );
+        }
+      }
+    );
+  }
+});
+
+app.post(
+  "/users/login",
+  passport.authenticate("local", {
+    successRedirect: "http://localhost:3001/todos",
+    failureRedirect: "/users/login",
+    failureFlash: true
+  })
+);
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("http://localhost:3001/todos");
+  }
+  next();
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("http://localhost:3001/todos");
+}
+
+//--------------------------------------------------------------------
+
+app.post("/todos", async (req, res) => {
+    try {
+      const { description } = req.body;
+      const { assigned } = req.body;
+      const { category } = req.body;
+      const newTodo = await pool.query(
+        "INSERT INTO todo (description, assigned, category, date) VALUES($1, $2, $3, current_date) RETURNING *",
+        [description, assigned, category]
+      );
+  
+      res.json(newTodo.rows[0]);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+  
+
+app.get("/todos", async (req, res) => {
+    try {
+      const allTodos = await pool.query("SELECT * FROM todo WHERE roles = $1", [role]);
+      res.json(allTodos.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+
+  app.get("/todos/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+        id
+      ]);
+  
+      res.json(todo.rows[0]);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+
+  app.put("/todos/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { description } = req.body;
+      const updateTodo = await pool.query(
+        "UPDATE todo SET description = $1 WHERE todo_id = $2",
+        [description, id]
+      );
+  
+      res.json("Todo was updated!");
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+
+  app.delete("/todos/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+        id
+      ]);
+      res.json("Todo was deleted!");
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
+
+//--------------------------------------------------------------------
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+function setRole(value){
+  role = value;
+}
+
+module.exports.setRole = setRole;
